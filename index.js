939476cd37bbc0b06c5b26ce37b1f44c51325215
shangsunset <shangsunset@gmail.com>
2016-03-09 23:10:37 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,52 @@
+'use strict'
+
+const spawn = require('child_process').spawn
+const net = require('net')
+const dns = require('dns')
+const fs = require('fs')
+
+const port = 4042
+const song = process.argv.slice(2)[0]
+
+if (song) {
+
+  const server = net.createServer(socket => {
+    const source = fs.createReadStream(song);
+    source.pipe(socket)
+
+    socket.on('error', error => {
+      console.log(`server socket error: ${error}`)
+      socket.destroy()
+    })
+  })
+
+  server.on('error', error => {
+    console.log(`server error: ${error}`)
+  })
+
+  server.listen(port)
+  connect('localhost')
+
+} else {
+ 
+  dns.lookup('Yeshens-MBP.local', (err, host) => {
+    connect(host)
+  })
+}
+
+function connect(host) {
+
+  const socket = net.connect(port, host)
+
+  const child = spawn('mplayer', ['-'], { stdio: [null, 'inherit', 'inherit'] })
+
+  socket.pipe(child.stdin)
+
+  socket.on('error', error => {
+    console.error(`client socket error: ${error}`)
+  })
+  child.on('error', error => {
+    console.log(`mplayer error: ${error}`)
+    process.exit(1)
+  });
+}
